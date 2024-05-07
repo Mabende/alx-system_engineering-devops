1eee2f036e98eb4a231c2e0675cba9e743ad19d6
Mabende <belindamabende@gmail.com>
2024-05-07 08:17:12 -0700

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# dOCS
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'set 301 redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/254guru permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
