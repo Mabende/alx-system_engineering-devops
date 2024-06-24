05386a9f0e04b29eeb771af166e427878be812ae
Mabende <belindamabende@gmail.com>
2024-06-24 11:06:29 -0700

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,36 @@
 """
 Contains the number_of_subscribers function
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if the
+        subreddit is invalid.
+    """
+    if subreddit is None or not isinstance(subreddit, str) or \
+       not subreddit.isidentifier():
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    try:
+        response = requests.get(
+            'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+        )
+
+        if response.status_code == 200:
+            data = response.json()
+            if 'data' in data and 'subscribers' in data['data']:
+                return data['data']['subscribers']
+
+    except requests.RequestException as e:
+        print("An error occurred: ", e)
+
+    return 0
